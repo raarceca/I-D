be2f37cf2b776d79d271b28f3d628495f47ee565
Ricardo Arce Campos <riarce94@gmail.com>
2015-04-13 02:25:46 -0600

    Manipulacion de usuarios

---
+++ TAREAS-GRUPALES/Central de Revelados Instantaneos/Proyecto/menu.php
@@ -0,0 +1,5 @@
+<div>
+    <center>
+        <a href="main.php">Home</a> | <a href="showUsers.php"> Mostrar Usuarios </a> | <a href="adduser.php">Add User</a> | <a href="modifyuser.php">Modify User</a> | <a href="deleteuser.php">Delete User</a> || Bienvenido: <?php echo $_SESSION['apellido1'] . ' ' . $_SESSION['apellido2'] . ', ' . $_SESSION['nombreUsuario']; ?> | <a href="logout.php">Logout</a>
+    </center>
+</div>
---
+++ TAREAS-GRUPALES/Central de Revelados Instantaneos/Proyecto/showUsers.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+require_once('lib/ConectorBD.php');
+$query = 'SELECT p.cedula, p.nombre, p.apellido1, p.apellido2, p.email, p.dob fechaNacimiento, u.usuario_id, u.clave
+FROM persona p
+JOIN usuario u ON p.usuario_id = u.usuario_id';
+$arreglo = array();
+$arreglo = ConectorBD::selectQuery($query);
+?>
+<html>
+<head>
+    <title>Mostrar Usuarios</title>
+</head>
+<body>
+<header>
+    <img src="img/Logo.png" width="7%" height="15%""> Main Page
+</header>
+<div><?php include('menu.php'); ?></div>
+<section>
+    <h3>Listado de Usuarios Activos</h3>
+    <table>
+        <tr>
+            <td> <b>Cedula</b> </td>
+            <td> <b>Nombre</b> </td>
+            <td> <b>Apellidos</b> </td>
+            <td> <b>E-Mail</b> </td>
+            <td> <b>Fecha de Nacimiento</b> </td>
+            <td> <b>Usuario</b> </td>
+        </tr>
+    <?php
+        foreach ($arreglo as $row) {
+            echo '<tr>';
+                echo '<td>'.$row['cedula'].'</td>';
+                echo '<td>'.$row['nombre'].'</td>';
+                echo '<td>'.$row['apellido1'].' '.$row['apellido2'].'</td>';
+                echo '<td>'.$row['email'].'</td>';
+                echo '<td>'.$row['fechaNacimiento'].'</td>';
+                echo '<td>'.$row['usuario_id'].'</td>';
+            echo '</tr>';
+        }
+    ?>
+    </table>
+</section>
+<footer>
+    <center>Copyright 2015. Developed by I&D</center>
+</footer>
+</body>
+</html>
