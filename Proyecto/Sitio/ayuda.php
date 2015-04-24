33e43d08f8a9f0e911442610b2b9c359708342bd
Christian Jimenez <iamchrisjim@gmail.com>
2015-04-24 12:25:02 -0600

    Agrega archivo de ayuda y otros complementarios

---
+++ Proyecto/Sitio/ayuda.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<div><?php include('menu.php'); ?></div>
+<html>
+<head>
+	<title>Ayuda</title>
+	<link rel="stylesheet" type="text/css" href="public/css/ayuda_estilos.css"/>
+</head>
+
+<body>
+	<div id="header">
+		<h1><img src="public/images/logo.png"/></h1>
+	</div>
+
+	<div id="elementos">
+		<h2>Ayuda</h2>
+		<ul>
+			<li><a href="#agregar_usuario">Agregar usuario</a></li>
+			<li><a href="#modificar_usuario">Modificar usuario</a></li>
+			<li><a href="#eliminar_usuario">Eliminar usuario</a></li>
+			<li><a href="#listar_usuarios">Listar usuarios</a></li>
+			<li><a href="#Configuracion_MV">Configuraci&oacuten de M&aacutequina Virtual</a></li>
+			<li><a href="#registro_pos">Registro de Punto de Venta</a></li>
+			<li><a href="#gestion_respaldos">Gesti&oacuten de Respaldos</a></li>
+		</ul>
+
+	<div class="ayuda" id="agregar_usuario">
+		<h3><img src="public/images/question_mark.png" alt=""/>Agregar usuario</h3>
+		<p>Se debe de llenar cada uno de los campos del formulario ya que todos son requeridos.</p>
+	</div>
+
+	<div class="ayuda" id="modificar_usuario">
+		<h3><img src="public/images/question_mark.png" alt=""/>Modificar usuario</h3>
+		<p>Se debe de ingresar el n&uacutemero de c&eacutedula del usuario a modificar para realizar la b&uacutesqueda. Seguidamente aparecer&aacuten los datos del usuario y ser&aacute posible modificarlos simplemente posicion&aacutendose sobre el campo y realizando el cambio.</p>
+	</div>
+
+	<div class="ayuda" id="eliminar_usuario">
+		<h3><img src="public/images/question_mark.png" alt=""/>Eliminar usuario</h3>
+		<p>Se debe de ingresar el n&uacutemero de c&eacutedula del usuario a eliminar para realizar la b&uacutesqueda. Una vez que se encuentre la coincidencia el usuario ser&aacute eliminado.</p>
+	</div>
+
+	<div class="ayuda" id="listar_usuarios">
+		<h3><img src="public/images/question_mark.png" alt=""/>Listar usuarios</h3>
+		<p>Permite listar todos los usuarios en el Sistema presentando un usuario por l&iacutenea y sus respectivos datos en estilo columna siguiendo la forma:</p>
+		<pre>Nombre | Primer_Apellido | Segundo_Apellido | Email | Fecha de Nacimiento  | Nombre de Usuario</pre>
+
+	<div class="ayuda" id="Configuracion_MV">
+		<h3><img src="public/images/question_mark.png" alt=""/>Configuraci&oacuten de M&aacutequina Virtual</h3>
+		<p>En Construcci&oacuten</p>
+	</div>
+
+	<div class="ayuda" id="registro_pos">
+		<h3><img src="question_mark.png" alt=""/>Registro de Punto de Venta</h3>
+		<p>En Construcci&oacuten</p>
+	</div>
+
+	<div class="ayuda" id="gestion_respaldos">
+		<h3><img src="public/images/question_mark.png" alt=""/>Gesti&oacuten de Respaldos</h3>
+		<p>En Construcci&oacuten</p>
+	</div>
+
+	<p class="top">
+		<a href="#">Volver Arriba</a>
+	</p>
+	 
+	<address>
+		Copyright &copy; 2015 
+		Infrastructure & Development.
+	</address>
+</body>
+</html>
+</php>
--- Proyecto/Sitio/menu.php
+++ Proyecto/Sitio/menu.php
@@ -3,6 +3,6 @@
     <a href="/Proyecto/Sitio/forms/Login.php">Login</a>
     | <a href="/Proyecto/Sitio/Respaldos.php">Listado Respaldos</a> |
     | <a href="/Proyecto/Sitio/forms/ConfiguracionMV.php">Configuracion MV</a>
-    | <a href="/Proyecto/Sitio/Ayuda.php">Ayuda</a>
+    | <a href="/Proyecto/Sitio/ayuda.php">Ayuda</a>
     | <a href="" onclick="alert('Falta implementar');"></a>
 </div>
